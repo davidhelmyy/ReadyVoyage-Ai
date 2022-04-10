1b933bf822961f483a12120ed9e0d5945e3244db
davidhelmy <david.helmy.ext@brightskiesinc.com>
2022-04-10 03:43:59 +0200

    readme added

--- PlaceApi_test.py
+++
@@ -1,7 +0,0 @@
-import googlemaps
-
-apiKey="AIzaSyCRe9Kyry9O0AFeM9IDGfmggxTjxQN8VQk"
-
-gmap=googlemaps.Client(key=apiKey)
-result=gmap.places_nearby(location="43.7768,11.2586",radius=40000,type='cafe')
-print(result)
